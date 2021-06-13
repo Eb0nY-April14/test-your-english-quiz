a4cea663704bacf4aae994eaf90d7a748e2d9fe1
Eb0nY-April14 <laratemi14@gmail.com>
2021-06-13 19:53:28 +0000

    Created functions structure for the quiz game

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+/* We need to avoid creating Global variables as much as possible because they can be overwritten
+ unknowingly so we are containing our variables within the different functions i.e local scopes so
+that's why functions come in handy */
+
+function runGame() {
+
+}
+
+function checkUserAnswer() {
+    
+}
+
+function giveSystemAnswer() {
+    
+}
+
+function incrementRightAnswerScore() {
+    
+}
+
+function incrementWrongAnswerScore() {
+    
+}
+
+function displayQuestion() {
+    
+}
+
