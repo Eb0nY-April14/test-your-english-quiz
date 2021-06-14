2884c6d90b0ccd621166a2be9db38395b0e75f36
Eb0nY-April14 <laratemi14@gmail.com>
2021-06-14 02:33:45 +0000

    Used template literals for inserting the questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,6 +17,8 @@ document.addEventListener("DOMContentLoaded", function()  {    //This waits for 
 })
 
 
+
+
 /* We need to avoid creating Global variables as much as possible because they can be overwritten
  unknowingly so we are containing our variables within the different functions i.e local scopes so
 that's why functions come in handy */
@@ -42,6 +44,114 @@ function incrementWrongAnswerScore() {
 }
 
 function displayQuestion() {
+
     
 }
 
+let question = document.getElementById("question-area");
+
+let questionHTML = ` [
+    {
+      question: "I kept your food on the ------------------.",
+      answers: {
+        a. "tabel",
+        b. "tablet",
+        c. "table",
+        d. "tabble"
+      },
+      correctAnswer: "c"
+    },
+    {
+      question: "She is a very ----------------------- lady.",
+      answers: {
+        a. "optimistik",
+        b. "optamistic",
+        c. "optimicite",
+        d. "optimistic"
+      },
+      correctAnswer: "d"
+    },
+    {
+        question: "An act of ---------------- is dangerous.",
+        answers: {
+          a: "rebellion",
+          b: "rebelion",
+          c: "rebbellion",
+          d: "rebelione"
+        },
+        correctAnswer: "a"
+      },
+      {
+        question: "The food I bought at the supermarket tasted ------------------------.",
+        answers: {
+          a: "disgusting",
+          b: "disgausten",
+          c: "disgorsting",
+          d: "disgustine"
+        },  
+        correctAnswer: "a"
+      },
+      {
+        question: "I am very ------------------ at the salesgirl's attitude.",
+        answers: {
+          a: "disapointed",
+          b: "disappointed",
+          c: "dissappointed",
+          d: "dicappointed"
+        },
+        correctAnswer: "b"
+      },
+      {
+        question: "My spouse's ---------------- party was fun.",
+        answers: {
+          a: "rebellion",
+          b: "rebelion",
+          c: "rebbellion",
+          d: "rebelione"
+        },
+        correctAnswer: "a"
+      },
+      {
+        question: "An act of ---------------- is dangerous",
+        answers: {
+          a: "rebellion",
+          b: "rebelion",
+          c: "rebbellion",
+          d: "rebelione"
+        },
+        correctAnswer: "a"
+      },
+      {
+        question: "An act of ---------------- is dangerous",
+        answers: {
+          a: "rebellion",
+          b: "rebelion",
+          c: "rebbellion",
+          d: "rebelione"
+        },
+        correctAnswer: "a"
+      },
+      {
+        question: "An act of ---------------- is dangerous",
+        answers: {
+          a: "rebellion",
+          b: "rebelion",
+          c: "rebbellion",
+          d: "rebelione"
+        },
+        correctAnswer: "a"
+      },
+      {
+        question: "An act of ---------------- is dangerous",
+        answers: {
+          a: "rebellion",
+          b: "rebelion",
+          c: "rebbellion",
+          d: "rebelione"
+        },
+        correctAnswer: "a"
+      }
+    ];
+
+
+`
